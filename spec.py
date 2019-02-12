e5f88c95e00853587a4773f4bdb433afc944d58a
Erno Palonheimo <esp@iki.fi>
2019-02-12 04:05:01 -0500

    a bunch of new features

--- spec.py
+++ spec.py
@@ -1,95 +1,56 @@
-import os
-import sys
-import pwd
-import grp
 import getopt
-from stat import *
+import glob
+import grp
+import os
+import pwd
+import shutil
+import subprocess
+import sys
+import tempfile
 
+from stat import *
+from pathlib import Path
+
+def err(string):
+    print(string, file=sys.stderr)
+
+def debug(string):
+    if verbose:
+        err(string)
+        
 def usage():
-    print('''
-::~:~:~:~:::~~~~~~:~::::::::::~:::,:,,,,,,,:,,,,,,,,,,,,,,,,,,,,,,...,,,,.,,,.,.
-::::::::::~~::~:~::::~::~::::::~:::,::=+????I?I=+,.:,,,,,..,,,,.,....,..,,,.,,,.
-:,:::::::~::~:~:::::::::::::::::::~+???+I?IIIIIIIII??==:,,,,,,.,..,,,,.,....,,,.
-:::::::~::::::~~:::::::::::::::::~=+,++I?I?II?IIIIIIII?I7II=,,,,,,,,,,..,,...,,.
-:::::::~:::~~::::::::::::~::~:~:~?=I=?~??II7777I7I7II??IIIII+,,,,,,,,,,,,,,,,,,.
-:::~:::::::::~:~::::::~::::~~~:+II+:I7II77777777777777I?I77I+:,,,,,,,,,,,,,,,,,.
-::::::~:~:~::::::::~~::::~:~~=?II?IIII777777777777777777I7777I,,,,,,,,,,.,,..,,.
-:::::::::::::::::::::::::~:~+II7+IIII7777777777777777777777777I,,,,,,.,.,..,,,,.
-:::::::::~::::::::~:~::~::~I7II7?+?II77777777777777777777777777I::,,,,,,,,,,,,,.
-:::::::::~:::~:::::~~~~~:~?II+??=+?III77777777777777777777777777?::,,,,,,,,,,,,.
-::::::::::::::::::::~~~~=?I?+:I~~=??I7777777777777777777777777777~:,,,,,,,,,,,,.
-:::::::::::::::::::::~:~~+?????::=?I77777777777777777777I7777777777~:,,,,,,,,,,.
-::::::::::::::::::::::::=I?III?,:???+I77777777?II77777777?77777777777=,,,,,,,,,.
-::::::::~::::::::::::~:~??=II?=:~~,...,?I+~::,..~==?77I77?II7I7I777II7?:,,,,,,,.
-::::::::::::::::::::::=?~++7I?::,:+?+,,..=I~,:,:IIII+II777=III7II77II=?I,,,,,,,.
-:::::::::::~:::::::~~+=+~,=II~.:,,.,..,=,I7II::~.+~.=I?I7I?I??II7IIII+II?:,,,,,.
-::::::::::::::::::::+?=++:I~:?~:,:+~~~++?I77III??I777I77I?I+I++IIIIII??+I?:,,,,.
-:::::::::~~::::::::~:+:=+==:~+:,=??I?II?I7777777I777777II?II:~+I?=I~II7++==,,,,.
-::::::::::::::,::::~~=~I=~????,,:?IIII==II7777I7777777II?:?,??II??:~??+?+=~=,,,.
-::::::::::::::::::~:~:+~~~:+==..,+?II=~..+=:IIII777777II?+I:~=+==?,~~+~~~::,,,,.
-::::::::::::::::::~==~~,,::=::..,=+==?,......=I7II777II?~+I???+,~:~~,~,:~,:,,,,.
-:::::::::~::::::::,~:,,,,,=:~:,.,,:....,,.,...,~+IIIII7II==???I+,~=~~~:=:,,:,,,.
-::::::~:~:~:::::::,,,,,.,=:~I:~:,:,...........,,.::??I=~II=I+~=.::.:,.,,~,:,,,,.
-::::~~~:~:~::::::,,,:,,,,=:,,~,.,...............,,:,~?,=??II7?,~:,,..,.,,.,,,,,.
-::::::::~::::::::::,,:,~:~=~:,,,,...................:~==??=I?I?,:..,,.,.,,,,,,,.
-::::~:~::::~:::::::,,,,,~,::,:~,,...~==::,,:,..,,...,~??++I=?I?=.....,,.,,,,,,,.
-::::::::::~::::::,:,,:~,,,::??,~~,,:~+:??=?++==~?=,:++=II?IIIII:,....,,,..,,,,,.
-~:::~:~~:::~:::::,:,,,,,,::+?+:+I?=+?I=+??IIIIIIII?~?+II7III7I?:+..,,,.,,,,,,,,.
-:::::::::~::~::::::::,,,::=?+=??+=??IIIIII=?I7777II??I7777?II7II=,..,,,,,,,.,,,.
-:~:::::::::~~:~::::::,::=I~=:????7?7II?:?~I?7I77?I77?II7I7I?III??:,,,,,,,,,,,,,.
-::~::::::~:~::::,,::::::=+=~:+==I77?I?7???I?I7IIIII77?II777II7?II=:,,,,,,,,,,,,.
-:::::::::::::::::::,::,:~=::=:~==?I??=?=~?=+I?II?II7IIIIIIII=???,.~,,,,,,,,.,,,.
-::::::::::::::::::,,,...,,:=?=++==?I?=+==+=I+??I?IIIII?IIII?,I:I:..,,,,,,,,,,,,.
-::,::::::::::::,.............=~+=?=+,+=:=?+?I++7I??II?????I,??.,......,,,,,,,,,.
-::::::::::::::..............,=~+===,+~,?~~:+++?==+?+?+==~:,,=,............,,,,,.
-:,::::,::::::.................~=:===~,?~++====~~:?++?+:.,:=,,...............,,,.
-:,:,::::,,::,.................+=+:::~:~:.:=,~+:,,,.,..........................,.
-,,,,::::,,:,................III?+=~~=,,,....,,.,.............................,,.
-,,:,,:,:::,...............~7IIII???+~~~~,..........................,,,...,..:~~,
-,,,:,,,:,...............,I7?77II7II???+.........................,:::,....,~~,,:,
-,,,::,:,....,..,,...,..~77I7777777III~........................,,:,,,,.,~==,.,~~,
-,,,:,,:..........,,,.:I7777777I7777I:..................,.....,,,..,,.:~=,.,~,,:,
-,,,:,:.,.........,..I777777777777II..,................,......,,.,,.,:=~,~=,,,~:.
-,,,,,,.,..,.,...,.,777777777777II:,.,.........,,,...,,.....,,,..,,,:::~=~======.
-,,,,,.........,,,I777777777777II.,.,.........,,,,,,,,....,,,,,..,,,:...,:~:::~..
-,,,,......,..,,:7777777 77777I..,,..,..,,..,,,,,,,,,,...,,,,.....,,..::~:~,.,~~.
-:::,,,..,....,,7777777777777?.,,,.,.....,.,,.,,,,,,..,,,..,.....,,.,::,,,::~=~:,
-::,..........,,7777777777II..,,,,,........,..,,,,,..,,,,.......,,..,.~,,::~::==,
-::,,,.........,I7777777I7..,...,,,,,,.....,,,,,,....,,.,......,...:,..,:~~~~~::.
-,,............,~7777777,.,,,,,,,,,,.,....,,,,,,.,,.............,:,...,,:~==~:~=,
-.,.,...........,777I,I..,,,,,,,,,,,,,,.,..,,..,,,,,..,.......,,,,...,,::~~~~:::,
-...............,?7?+,.,,,..,,,,,,,,,,,,.,,..,,,,,,..........,,,...,,,:~~=~=====:
-..................I....,,,,,,,,,,,,,,,,...,,.,,,,,..........,....,,,~~:~~~=++++,
-.............,:.+~...,,....,,,,,,,,,,,....,.,,,.,,,.......,.,..,,,:~==:~::~=?+:.
-........,IIII=II~....,.....,,,,,,,,............................,,,~~~::~::~=~,,.
-.......II?III,,+......,,,...,.,,,...............................::~~~~~=~~:,,,,.
-.....,?I?IIIIII...,,,,,,...,,,...............................,.,:~~+=:,,,,,..,,.
-....,,I+IIIIII.......,..........,.............................,::,:=,,,,,,::,::,
-..,,,II=IIIII:.,....,,.,........,,,..........,,,,...........,,::~:,.,:~~~~=~:~:,
-,,,,,II~+III?,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,~=~~~~=~~~~====+++:
-Karl Marx (1818-1883)
-''')
-    
-    print("Usage: %s (options) <paths>" % sys.argv[0])
-    print(" -d<dir>     create destination paths relative to dir")
-    print(" -u<uid>     force uid on all files")
-    print(" -g<gid>     force gid on all files")
-    sys.exit(0)
+    err("Usage: %s (options) <paths>" % sys.argv[0])
+    err(" -d<dir>     create destination paths relative to dir")
+    err(" -u<uid>     force uid on all files")
+    err(" -g<gid>     force gid on all files")
+    err(" -h<header>  use this file as package header")
+    err(" -p<name>    package name")
+    err(" -t          run epm to create a tardist")
+    err(" -v          verbose output")
+    err(" -e          output the generated tardist name to stdout after completion")
+    sys.exit(2)
 
 if len(sys.argv) < 2:
     usage()
     
-paths = sys.argv[1:]
-
 force_uid = None
 force_gid = None
 strip_prefix = None
+header_file = None
+package_name = None
+run_epm = False
+workdir = None
+output_fd = None
+output_fname = None
+verbose = False
+symlinks = []
+echo_tardist = False
 
 try:
-    opts, args = getopt.getopt(sys.argv[1:], "u:g:d:", ["help", "output="])
-except getopt.GetoptError as err:
+    opts, args = getopt.getopt(sys.argv[1:], "u:g:d:h:p:tv", ["help", "output="])
+except getopt.GetoptError as getopt_err:
     # print help information and exit:
-    print(err)  # will print something like "option -a not recognized"
+    err(getopt_err)  # will print something like "option -a not recognized"
     usage()
 
 for o, a in opts:
@@ -99,6 +60,18 @@ for o, a in opts:
         force_uid = int(a)
     elif o == '-d':
         strip_prefix = a
+    elif o == '-h':
+        header_file = a
+    elif o == '-p':
+        package_name = a
+    elif o == '-t':
+        run_epm = True
+    elif o == '-v':
+        verbose = True
+    elif o == '-e':
+        echo_tardist = False
+
+paths = args
 
 def get_info(path, known_type=''):
     src = path
@@ -125,11 +98,73 @@ def get_info(path, known_type=''):
         type = 'f'
     return(type, oct(mode)[2:], owner, group, dst, src)
 
+def output(line):
+    if run_epm:
+        print(line, file=output_fd)
+    else:
+        print(line)
+
+if run_epm:
+    if package_name is None:
+        err("With -t you need to specify a package name with -p")
+        usage()
+    workdir = tempfile.mkdtemp(suffix='-specpy')
+    debug("workdir %s" % workdir)
+    output_fname = workdir + '/' + package_name + '.list'
+    debug("output file %s" % output_fname)
+    try:
+        output_fd = open(output_fname, 'w+')
+    except Exception as e:
+        err("can not create %s: %s" % (output_fname, e))
+        sys.exit(2)
+
+if header_file is not None:
+    with open(header_file) as f:
+        # TODO: grab LICENSE and README and symlink to workdir to avoid absolute paths?
+        output(f.read())
+        
 while len(paths):
-    path = paths.pop()
+    path = os.path.abspath(paths.pop())
+    debug("processing %s" % path)
     for root, dirs, files in os.walk(path):
-        print("%s %s %s %s \"%s\" \"%s\"" % get_info(root, known_type='d'))
+        firstpart = Path(root).parts[1]
+        debug("entering %s" % root)
+        if firstpart not in symlinks:
+            symlinks.append(firstpart)
+        output("%s %s %s %s \"%s\" \"%s\"" % get_info(root, known_type='d'))
         for file in files:
             fpath = root + '/' + file
-            print("%s %s %s %s \"%s\" \"%s\"" % get_info(fpath))
+            output("%s %s %s %s \"%s\" \"%s\"" % get_info(fpath))
 
+output_fd.close()
+            
+if run_epm:
+    for path in symlinks:
+        src = '/' + path
+        dst = workdir + '/' + path
+        debug("symlinking %s to %s" % (src, dst))
+        os.symlink(src, dst)
+    args = [
+        'epm',
+        '-f', 'tardist',
+        package_name
+    ]
+    if verbose:
+        args.append('-v')
+    debug("running %s" % ' '.join(args))
+    cp = subprocess.Popen(args, cwd=workdir)
+    cp.wait()
+    debug("epm done")
+    if cp.returncode:
+        err("non-zero epm exit status: %s" % ' '.join(args))
+        sys.exit(2)
+    tardist = glob.glob(workdir + '/irix-6.5-mips/*.tardist')
+    if len(tardist) == 0:
+        err("epm set no error code but tardist is missing?")
+    if len(tardist) > 1:
+        err("more than one tardist found?")
+    shutil.copy(tardist[0], '.')
+    if echo_tardist:
+        print(tardist)
+    debug("clean up %s" % workdir)
+    shutil.rmtree(workdir)
